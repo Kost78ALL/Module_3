6a887d6f143ed6040e06d18f77495cc43f6c700b
Фоксик <79052688528@yandex.ru>
2025-01-12 13:14:18 +0300

    06,01

--- module_3_4.py
+++ module_3_4.py
@@ -1,9 +1,17 @@
-def single_root_words(root_word, rw = None, ow = None, same_words = None, *other_words):
-    if rw = None:
-        rw = root_word.lower
-        ow = other_words.lower
+def single_root_words(root_word, *other_words, rw = None, ow = None, same_words = None):
+    if rw == None and ow == None and same_words == None:
+        rw = []
+        ow = []
+        rw = str(root_word).lower()
+        ow = list(other_words)
         same_words = []
         for i in range(len(other_words)):
+            if rw in ow[i].lower():
+                same_words.append(ow[i])
+            elif ow[i].lower() in rw:
+                same_words.append(ow[i])
+
+        return same_words
 
 
 
@@ -11,10 +19,9 @@ def single_root_words(root_word, rw = None, ow = None, same_words = None, *other
 
 
 
-result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+
+result1 = single_root_words('Rich', 'ricHiest', 'Orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
 print(result2)
-
-
 
