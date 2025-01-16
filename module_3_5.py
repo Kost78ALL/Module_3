984098db2e370e4d677fbc52958ae49c320ab8c2
Фоксик <79052688528@yandex.ru>
2025-01-16 17:16:11 +0300

    06,01

--- module_3_5.py
+++ module_3_5.py
@@ -3,8 +3,7 @@ def get_multiplied_digits(number):
     #print(len(str_number))
     first = int(str_number[0])
     if len(str_number) <= 1:
-        return first
-    second = int(str_number[1:])
+        return first if first != 0 else 1
     return first * get_multiplied_digits(int(str_number[1:]))
 
 
@@ -16,5 +15,3 @@ result = get_multiplied_digits(40203)
 print(result)
 result2 = get_multiplied_digits(402030)
 print(result2)
-result3 = get_multiplied_digits(402031)
-print(result3)
